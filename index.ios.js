31468005ebf7cca19fe1fd4cb5d454abee97022e
PHampy <philiphampy@autozone.com>
2017-10-09 12:48:37 -0500

    removed test

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native'
 import App from './src/components/App'
 
-AppRegistry.registerComponent('MatchAndMentord', () => App)
+AppRegistry.registerComponent('MatchAndMentor', () => App)
